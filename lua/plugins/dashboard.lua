cb071b34558474b4a146a3517b5eff1ab1f48717
Aiden Roberts <buckion@users.noreply.github.com>
2025-07-31 11:17:14 +1000

    upgraded to v5

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = table.concat({
+          "Aiden's Kitchen",
+          "Cooking up AI",
+        }, "\n"),
+      },
+    },
+  },
+}
