a61b80b0dcc6c174cb0c6cbce2a112b158adaae7
Aiden Roberts <buckion@users.noreply.github.com>
2026-04-29 14:45:41 -0500

    upgraded to v5

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,6 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.python.ruff" },
   -- import/override with your plugins folder
 }
