83517f3980d07ae2f26179986b0454ffc69e60ce
Aiden Roberts <buckion@users.noreply.github.com>
2026-05-27 18:01:41 +1000

    added more tools

--- lua/community.lua
+++ lua/community.lua
@@ -12,5 +12,14 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.python.ruff" },
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.ty" },
+  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.just" },
   -- import/override with your plugins folder
 }
